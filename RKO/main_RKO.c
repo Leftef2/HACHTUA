b381dff138ecdc0beb3e03f41d9980471757f94c
Leftef2 <alexander.love@students.plymouth.ac.uk>
2025-01-30 13:07:13 +0000

    working timers

--- RKO/main_RKO.c
+++ RKO/main_RKO.c
@@ -10,64 +10,53 @@
 #include "usart.h"
 #include "my_switch.h"
 #include "my_buzzer.h"
+#include "timers.h"
 
 int main(void){
 	int A;
+	int K;
 	initLCD();
 	ADC_SETUP("PA0",1);//sets up PA0 and selects which ADC to use
 	LED_SETUP("PB0");
 	LED_SETUP("PB7");
 	LED_SETUP("PB14");
+	LED_SETUP("PB13");
 	clr_LCD_RS();
 	init_USART(3,"PD8","PD9");
-	createSwitch("PC13");
-
+//	createSwitch("PG0");
+	timer_init();
 	
 	char Sout[20];
 	while(1){
-		send_usart(3,'A');
-		ADCstartconv(1);				//start ADC conversion
-		sprintf(Sout,"output is= %5.i\n",ADCout(1));
-
-		cmdLCD(LCD_LINE1);
-		for(A=0;A<sizeof(Sout);A++){
-			WaitLcdBusy();
-			putLCD(Sout[A]);
-		};
-		sprintf(Sout,"                  \n");
-		cmdLCD(LCD_LINE2);
-		for(A=0;A<sizeof(Sout);A++){
-			WaitLcdBusy();
-			putLCD(Sout[A]);
-		};
-
-		
-		int Z;
-		while(Switch("PC13")){
-				Z=!Z;
-				if(Z==1){
-					BUZZER_ON("PB13");
-				}
-				else{
-					BUZZER_OFF("PB13");
-				}
+		TIM2_IRQHandler();
+//		ADCstartconv(1);
+		K=!K;
+		if(K){
+			LED_ON("PB13");
+			LED_ON("PB0");
+		}
+		else{
+			LED_OFF("PB13");
+//			LED_OFF("PB0");
+		}
+//		if(Switch("PG0")==1){
+//			if(1366>ADCout(1)){
+				//LED_ON("PB0");
+	//		}
+//			else{
+				//LED_OFF("PB0");
+//			}
+			if(1366<ADCout(1)&ADCout(1)<2732){
+				LED_ON("PB7");
 			}
-		if(!Switch("PC13")){
-			BUZZER_OFF("PB13");
+			else{
+				LED_OFF("PB7");
+			}
+			if(ADCout(1)>2732){
+				LED_ON("PB14");
+			}
+			else{
+				LED_OFF("PB14");
+			}	
 		}
-		if(1366<ADCout(1)&ADCout(1)<2732){
-			LED_ON("PB7");
-		}
-		else{
-			LED_OFF("PB7");
-		}
-		if(ADCout(1)>2732){
-			LED_ON("PB14");
-		}
-		else{
-			LED_OFF("PB14");
-		}	
-		
-
-	}
 }
---
+++ RKO/timers.c
@@ -0,0 +1,22 @@
+#include "timers.h"
+#include "PLL_Config.c"
+void timer_init(void){
+	RCC->APB1ENR|=RCC_APB1ENR_TIM2EN;		//timer 2 clock enabled
+	TIM2->DIER|=TIM_DIER_UIE;						//timer uptdate interrupt enabled
+																			//APB clock is Fcy/2 = 180MHz/2 = 90MHz
+	TIM2->PSC=256-1;										//divide APB clock by 256 = 90MHz/256 = 351kHz
+	TIM2->ARR=35156;										//35156 counter reload value, gives a timer period of 100ms when F_APB = 90MHz and PSC = 256
+	TIM2->CNT=0;												//zero timer counter
+	NVIC->ISER[0]|=(1u<<28);						//timer 2 global interrupt enabled
+	TIM2->CR1|=TIM_CR1_CEN;							//start timer counter
+	PLL_Config();									// Set system clock to 180MHz
+	SystemCoreClockUpdate();			// Update SystemCoreClock
+
+}
+void TIM2_IRQHandler(void)			//TIMER 2 INTERRUPT SERVICE ROUTINE
+{
+	TIM2->SR&=~TIM_SR_UIF;				//clear interrupt flag in status register
+	GPIOB->ODR^=(1u<<0);								//XOR GPIOB output data register to invert the selected pin
+
+}
+	
