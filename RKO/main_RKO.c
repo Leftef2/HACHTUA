602b72a7dc10f11ee38354610a94cb80d5900811
Leftef2 <alexander.love@students.plymouth.ac.uk>
2025-02-24 15:27:09 +0000

    working BPM

--- RKO/main_RKO.c
+++ RKO/main_RKO.c
@@ -49,44 +49,59 @@ void send_Line1(char* str){
 }
 void send_Line2(char* str){
 	cmdLCD(LCD_LINE2);
-	for(int A=0;strlen(str);A++){
-		putLCD(str[A]);
-		if(strlen(str)-1==A){
+	for(int A=0;16;A++){
+		if(A<strlen(str)){
+			putLCD(str[A]);
+		}
+		else{
+			putLCD(' ');
+		}
+		if(A==16){
 			break;
 		}
 	}
 }
+//void send_Line2(char* str){
+//	cmdLCD(LCD_LINE2);
+//	for(int A=0;strlen(str);A++){
+//		putLCD(str[A]);
+//		if(strlen(str)-1==A){
+//			break;
+//		}
+//	}
+//}
 
 int main(void){
-	timer_init();
 	initLCD();
+	//send_Line1("Loading system...");
 	LED_SETUP("PB0");
-	send_Line1("Loading system...");
-	//createSwitch("PG0");
-	//createSwitch("PG2");
-	//createSwitch("PG3");
-	LED_ON("PB0");
+	createSwitch("PG0");
+	createSwitch("PG2");
+	createSwitch("PG3");
+	ADC_SETUP("PA0",1);
+	timer_init();
 	while(1){
 		;
 	}
 }
 int Counter=0;
-int A=0;
-int MenuPos=0;
+int Counter2=0;
 int Wait=0;
 int Scroll=0;
-int Selected=0;
 int choice=0;
 int ActiveChoice=0;
+int calibrating=1;
+int minSample=10000;
+int maxSample=0;
+char temp[17];
 void TIM2_IRQHandler(void)			//TIMER 2 INTERRUPT SERVICE ROUTINE -- 120 FPS Loop --
 {
 	TIM2->SR&=~TIM_SR_UIF;				//clear interrupt flag in status register
-	Counter++;
 	//send_Line1("  bpm/o2 level  ");  MenuPos=0
 	//send_Line1("   CPR assist   ");  MenuPos=1
 	//send_Line1("     options    ");  MenuPos=2
 	//send_Line1("     credits    ");  MenuPos=3
-	//if(ActiveChoice==0){
+	if(ActiveChoice==0){
 		if(Wait==0){
 			if(Switch("PG2")){
 				Scroll=Scroll+1;
@@ -94,11 +109,11 @@ void TIM2_IRQHandler(void)			//TIMER 2 INTERRUPT SERVICE ROUTINE -- 120 FPS Loop
 			if(Switch("PG0")){
 				Scroll=Scroll-1;
 			}
-			if(Scroll>7){
+			if(Scroll>=7){
 				Scroll=0;
 			}
 			if(Scroll<0){
-				Scroll=7;
+				Scroll=6;
 			}
 			if(Switch("PG3")){
 				ActiveChoice=choice;
@@ -142,11 +157,51 @@ void TIM2_IRQHandler(void)			//TIMER 2 INTERRUPT SERVICE ROUTINE -- 120 FPS Loop
 			send_Line1("   --credits--  ");
 			send_Line2("                ");
 			choice=4;
-		}	
-	//}
-	//if(ActiveChoice==1){
-	//	send_Line1("BPM:");
-	//	send_Line2("o2:");
-	//}
+		}
+	}		
+	if(ActiveChoice==1){//goes into bpm/o2 mode
+		ADCstartconv(1);
+		if(calibrating==1){
+			send_Line1("BPM:calibrating");
+			send_Line2("o2:calibrating");
+			Counter++;
+			if(ADCout(1)<minSample){
+				minSample=ADCout(1);
+			}
+			if(ADCout(1)>maxSample){
+				maxSample=ADCout(1);
+			}
+			if(Counter>=360){
+				calibrating=0;
+				Counter=0;
+			}
+		}
+		else{
+			int BPMtime;
+			if(0<Counter&&Counter<5){
+				Counter2++;
+			}
+			if(Wait==0){
+				if(ADCout(1)>(minSample+(0.75*(maxSample-minSample)))){
+					Counter++;
+					if(Counter==5){
+						LED_ON("PB0");
+						Counter=0;
+						sprintf(temp,"BPM:%1.i|",(int)round((Counter2*0.1)));
+						send_Line1(temp);
+						Counter2=0;
+					}
+					else{
+						LED_OFF("PB0");
+					}
+					Wait=1;
+				}
+			}
+			if(ADCout(1)<(minSample+(0.5*(maxSample-minSample)))){
+				Wait=0;
+			}
+			//send_Line2("o2:");
+		}
+	}
 }
 	
